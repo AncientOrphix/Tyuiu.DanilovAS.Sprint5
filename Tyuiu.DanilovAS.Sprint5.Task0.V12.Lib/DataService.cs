6fc66522b2e663b72c398d2d52c6f06b17536285
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2024-10-08 11:04:38 +0500

    Task0

--- Tyuiu.DanilovAS.Sprint5.Task0.V12.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint5.Task0.V12.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.DanilovAS.Sprint5.Task0.V12.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Path.GetTempPath()}\OutPutFileTask0.txt";
+            string path = $@"{Path.Combine("OutPutFileTask0.txt")}";
             double y = (Math.Pow(x,3)/(2*Math.Pow(x+5,2)));
             y = Math.Round(y,3);
             File.WriteAllText(path,Convert.ToString(y));
