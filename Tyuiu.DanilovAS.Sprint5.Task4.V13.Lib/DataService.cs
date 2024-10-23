56e29c61ed3d000cb88bbfd30ddfa24c2e8e4812
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2024-10-23 15:19:03 +0500

    Task4

--- Tyuiu.DanilovAS.Sprint5.Task4.V13.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint5.Task4.V13.Lib/DataService.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.DanilovAS.Sprint5.Task4.V13.Lib
             
 
             double x = Convert.ToDouble(strX);
-            double res = Math.Cos(x) + (Math.Pow(x,2)/(2));
+            double res = Math.Cos(x) + Math.Pow(x,2)/2;
             res = Math.Round(res, 3);
 
             return res;
