e7587dbee9d6e3552fd4295f64d5d8215a7959d3
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2024-10-23 17:50:14 +0500

    Task6

--- Tyuiu.DanilovAS.Sprint5.Task6.V2.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint5.Task6.V2.Lib/DataService.cs
@@ -9,14 +9,13 @@ namespace Tyuiu.DanilovAS.Sprint5.Task6.V2.Lib
             int count = 0;
             using (StreamReader reader = new StreamReader(path))
             {
-                string line;
-                
+                string? line;
                 while ((line = reader.ReadLine()) != null)
                 {
                     for(int i = 0; i < line.Length; i++)
                     {
                         //string russian = @"^[А-Яа-яЁё]+$";
-                        if ((line[i] >='A') && (line[i] <='я') || line[i]=='Ё' || line[i]=='ё')
+                        if (((line[i] >='A') && (line[i] <='я')) || line[i]=='Ё' || line[i]=='ё')
                         {
                             count++;
                         }
