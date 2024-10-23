ed54949fc9a7b65000ea7a9fbbc2b1a0578c9452
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2024-10-23 16:40:28 +0500

    Task5

--- Tyuiu.DanilovAS.Sprint5.Task5.V10.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint5.Task5.V10.Lib/DataService.cs
@@ -7,15 +7,15 @@ namespace Tyuiu.DanilovAS.Sprint5.Task5.V10.Lib
         public double LoadFromDataFile(string path)
         {
             double res = 0;
-
             using (StreamReader reader = new StreamReader(path))
             {
                 string? line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    if (Convert.ToDouble(line) %2 == 0)
+                    string newLine = line.Replace('.', ',');
+                    if (Convert.ToDouble(newLine) % 2 == 0)
                     {
-                        res = res + Convert.ToDouble(line);
+                        res = res + Convert.ToDouble(newLine);
                     }
                 }
             }
